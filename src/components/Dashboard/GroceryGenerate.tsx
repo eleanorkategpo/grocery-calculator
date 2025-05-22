6c85212144c5a0fd30b13bd2f9affd10fe742a56
Kate <eleanorkategpo@gmail.com>
2025-05-22 23:56:02 +0800

    Trigger deployment

--- src/components/Dashboard/GroceryGenerate.tsx
+++ src/components/Dashboard/GroceryGenerate.tsx
@@ -18,7 +18,7 @@ import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import LoadingOverlay from "../shared/LoadingOverlay";
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL; 
 
 const GroceryGenerate = () => {
   const navigate = useNavigate();
